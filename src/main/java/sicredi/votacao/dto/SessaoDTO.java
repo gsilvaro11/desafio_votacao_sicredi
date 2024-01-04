ce6bd41ce7f2dc5a80ddc5f00cdc81180bce1b22
gsilvaro11 <guiuser1999@gmail.com>
2024-01-04 02:30:05 -0300

    Retonando PautaDTO para a SessaoDTO

--- src/main/java/sicredi/votacao/dto/SessaoDTO.java
+++ src/main/java/sicredi/votacao/dto/SessaoDTO.java
@@ -3,12 +3,11 @@ package sicredi.votacao.dto;
 import java.sql.Timestamp;
 
 import lombok.Data;
-import sicredi.votacao.entity.PautaEntity;
 
 @Data
 public class SessaoDTO {
     private Long id;
     private Long duracao;
     private Timestamp dataCriacao;
-    private PautaEntity pauta;
+    private PautaDTO pauta;
 }
